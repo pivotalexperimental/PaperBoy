c0e74abc8efd8cea237dc1503f124f50fc06872f
Alex Basson and Robbie Clutton <pair+abasson+rclutton@pivotallabs.com>
2013-08-19 11:38:01 -0400

    Initial Commit

---
+++ PaperBoy-Spike/PBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PBAppDelegate.h
+//  PaperBoy-Spike
+//
+//  Created by pivotal on 8/19/13.
+//  Copyright (c) 2013 Pivotal Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PaperBoy-Spike/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PaperBoy-Spike
+//
+//  Created by pivotal on 8/19/13.
+//  Copyright (c) 2013 Pivotal Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PBAppDelegate class]));
+    }
+}
