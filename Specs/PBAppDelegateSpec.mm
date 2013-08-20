d4e11fdd382319fbd51369e9a37d6a1763eeb063
Alex Basson and Dan Grippi <pair+abasson+grippi@pivotallabs.com>
2013-08-20 11:53:06 -0400

    Test-drive PaperBoy app delegate

--- Specs/PBAppDelegateSpec.mm
+++ Specs/PBAppDelegateSpec.mm
@@ -10,22 +10,40 @@ describe(@"PBAppDelegate", ^{
 
     beforeEach(^{
         delegate = [[[PBAppDelegate alloc] init] autorelease];
-        [delegate application:nil didFinishLaunchingWithOptions:nil];
     });
 
-    if([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-        context(@"when on iPad", ^{
-            it(@"should use a UISplitViewController", ^{
-                [[delegate window] rootViewController] should be_instance_of([UISplitViewController class]);
+    describe(@"-application:didFinishLaunchingWithOptions:", ^{
+        beforeEach(^{
+            [delegate application:nil didFinishLaunchingWithOptions:nil];
+        });
+        
+        if([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
+            context(@"when on iPad", ^{
+                it(@"should use a UISplitViewController", ^{
+                    [[delegate window] rootViewController] should be_instance_of([UISplitViewController class]);
+                });
+            });
+        } else {
+            context(@"when on iPhone", ^{
+                it(@"should use a UINavigationController", ^{
+                    [[delegate window] rootViewController] should be_instance_of([UINavigationController class]);
+                });
+            });
+        }
+        
+        context(@"application receives a push notification", ^{
+            beforeEach(^{
+                NSDictionary *launchOptions = @{
+                    UIApplicationLaunchOptionsRemoteNotificationKey: @{@"content-available": @1}
+                };
+                [delegate application:nil didFinishLaunchingWithOptions:launchOptions];
+            });
+            
+            it(@"should download the new issue", ^{
+                
             });
         });
-    } else {
-        context(@"when on iPhone", ^{
-            it(@"should use a UINavigationController", ^{
-                [[delegate window] rootViewController] should be_instance_of([UINavigationController class]);
-            });
-        });
-    }
+    });
 });
 
 SPEC_END
